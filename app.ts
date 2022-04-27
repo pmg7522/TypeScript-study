cb954c91570d367149140cf966612217bbf22247
pmg7522 <pmg752290@gmail.com>
2022-04-27 13:47:10 +0900

    서버 연결 완료

--- app.ts
+++ app.ts
@@ -9,6 +9,10 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
   })
 })
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  return res.status(404).send({ message: "API를 확인해주세요." });
+});
+
 app.listen(port, () => {
   console.log(`서버가 ${port}로 실행중입니다.`);
 })
