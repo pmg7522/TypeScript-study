72a1395c40b6882d2f39bdc6d4997959ac4718c0
pmg7522 <pmg752290@gmail.com>
2022-08-23 09:49:23 +0900

    초기 설정중

--- app.ts
+++ app.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, NextFunction } from "express";
-import { sequelize } from "./models";
 import cors from "cors";
 import dotenv from "dotenv"
 
@@ -15,22 +14,13 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).send({
     message: "Welcome"
-  })
-})
+  });
+});
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   return res.status(404).send({ message: "API를 확인해주세요." });
 });
 
-
 app.listen(port, host, async () => {
   console.log(`서버가 ${host}:${port}로 실행중입니다.`);
-
-  await sequelize.sync()
-    .then(() => {
-      console.log("db 연결 성공");
-    })
-    .catch((err) => {
-      console.log("db 연결 실패", err);
-    })
 })
