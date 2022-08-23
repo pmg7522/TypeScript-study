85f5af8cc56fa3cdbefa96ff9e8b13139f3bd804
pmg7522 <pmg752290@gmail.com>
2022-08-23 10:37:08 +0900

    db 설정

--- config/config.ts
+++ config/config.ts
@@ -1,16 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import mysql from "mysql2";
 
-const {
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_NAME
-} = process.env
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: 3306,
+});
 
-export const config = {
-  development: {
-    username: DB_USERNAME || "root",
-    password: DB_PASSWORD,
-    database: DB_NAME || "typescript"
-  }
-}
+export const db = pool.promise();
