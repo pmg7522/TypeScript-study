3e70f129e243e6c2b15497d94d88ec383a1693fb
pmg7522 <pmg752290@gmail.com>
2022-08-25 14:23:32 +0900

    API 연결 테스트중

--- app.ts
+++ app.ts
@@ -1,24 +1,34 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import dotenv from "dotenv"
+import morgan from "morgan";
+import router from "./route"
+
+const app = express();
 
 dotenv.config();
 
 const port: number = parseInt(process.env.SERVER_PORT as string, 10) || 3000;
 const host: string = process.env.SERVER_URL || 'localhost';
-const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
+
+
+app.use(morgan(":method :url | :status | :response-time ms | :date[iso] | "));
 app.use(express.json());
 
+app.use("/", router);
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  return res.status(200).send({
-    message: "Welcome"
-  });
+  res.status(200).send({ message: "Welcome" });
 });
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  return res.status(404).send({ message: "API를 확인해주세요." });
+  res.status(404).send({ message: "API를 확인해주세요." });
 });
 
 // 에러처리
@@ -31,6 +41,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 //   })
 // });
 
-app.listen(port, host, async () => {
+app.listen(port, host, () => {
   console.log(`서버가 ${host}:${port}로 실행중입니다.`);
 });
