aed9735c9670935bb71882f3408ca540141d0577
pmg7522 <pmg752290@gmail.com>
2022-09-06 17:23:50 +0900

    회원가입 완료

--- controller/user/signup.ts
+++ controller/user/signup.ts
@@ -1,5 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { signUp } from "../../data/user";
 
 export const userSignUp = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(1);
+  const { username, password }: { username: string, password: string } = req.body;
+
+  const signUpData = { username, password };
+
+  signUp(signUpData)
+    .then(() => {
+      return res.status(200).send({
+        message: "회원가입에 성공하였습니다."
+      });
+    })
+    .catch((err) => {
+      return res.status(400).send({
+        message: "회원가입에 실패하였습니다."
+      });
+    });
 }
--- data/user.ts
+++ data/user.ts
@@ -1,12 +1,13 @@
 import { db } from "../config/config";
 import { UserSignUp } from "../model/user";
 
-const userSignUp = async function (userInfo: UserSignUp) {
+export const signUp = async function (userInfo: UserSignUp) {
   const { username, password } = userInfo;
 
   const sql: string = "INSERT INTO User(`username`, `password`) VALUES(?, ?)";
   const params: Array<string> = [username, password];
 
   const userId: any = await db.query(sql, params);
-  console.log(userId);
+
+  return userId;
 }
