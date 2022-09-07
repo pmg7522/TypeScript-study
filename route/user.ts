f21091df132ae0bd3e1b960874f05c92a8dca0f8
pmg7522 <pmg752290@gmail.com>
2022-09-07 10:24:12 +0900

    유저 로그인 작업중

--- route/user.ts
+++ route/user.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import {
-  userSignUp
+  userSignUp,
+  userLogin,
 } from "../controller";
 
 const router = express.Router();
 
 router.post("/signup", userSignUp);
+router.post("/login", userLogin);
 
 export default router;
