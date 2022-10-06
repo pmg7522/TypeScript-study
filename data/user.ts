660f74567931186ad565f8f516147583d9771a0c
pmg7522 <pmg752290@gmail.com>
2022-10-06 16:59:09 +0900

    feat: 유저 목록 및 정보 API 작업중
    Query 작업중

--- data/user.ts
+++ data/user.ts
@@ -1,5 +1,5 @@
 import { db } from "../config/config";
-import { UserSignUp, loginData } from "../model/user";
+import { UserSignUp, LoginData, UserInfo, UserList } from "../model/user";
 
 export const signUp = async function (userInfo: UserSignUp) {
   const { username, password } = userInfo;
@@ -12,7 +12,26 @@ export const signUp = async function (userInfo: UserSignUp) {
   return;
 }
 
-export const login = async function (loginData: loginData) {
+export const login = async function (loginData: LoginData) {
+  const { username, password } = loginData;
+
+  const sql: string = "SELECT username, password FROM User WHERE username = ? AND password = ?";
+  const params: Array<String> = [username, password];
+
+  const userData = await db.query(sql, params);
+
+  return userData;
+}
+
+export const getList = async function (userList: UserList) {
+  const sql: string = "SELECT username, password FROM User WHERE status = 0";
+
+  const userData = await db.query(sql);
+
+  return userData;
+}
+
+export const getDetail = async function (loginData: loginData) {
   const { username, password } = loginData;
 
   const sql: string = "SELECT username, password FROM User WHERE username = ? AND password = ?";
